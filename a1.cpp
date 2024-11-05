9fa9da47e9a1d949c2833f9e08aeac24f26fda29
asj317 <131603870+asj317@users.noreply.github.com>
2024-11-05 22:53:44 +0530

    Updated

--- a1.cpp
+++ a1.cpp
@@ -1,35 +1,44 @@
+//Fibonacci recursive 
+
 #include <iostream>
 using namespace std;
 
-int fibo(int n) {
-    if (n <= 1)
-        return n; 
-    return fibo(n - 1) + fibo(n - 2);
-}
-
-int fib(int n) {
+int fibonacci_recursive(int n) {
     if (n <= 1)
         return n;
-    
-    int prev1 = 0, prev2 = 1;
-    int f = 0;
-
-    for (int i = 2; i <= n; i++) {
-        f = prev1 + prev2;
-        prev1 = prev2;
-        prev2 = f;
-    }
-
-    return f;
+    return fibonacci_recursive(n - 1) + fibonacci_recursive(n - 2);
 }
 
 int main() {
     int n;
-    cout << "Enter the value of n: ";
+    cout << "Enter a number: ";
     cin >> n;
-
-    cout << "Fibonacci number (Recursive) is: " << fibo(n) << endl;
-    cout << "Fibonacci number (Non-recursive) is: " << fib(n) << endl;
-    return 0;
+    cout << "Fibonacci(" << n << ") = " << fibonacci_recursive(n) << endl;
+    return 0;
 }
 
+//Fibonacci non recursive 
+
+#include <iostream>
+using namespace std;
+
+int fibonacci_iterative(int n) {
+    if (n <= 1)
+        return n;
+    
+    int a = 0, b = 1, result;
+    for (int i = 2; i <= n; i++) {
+        result = a + b;
+        a = b;
+        b = result;
+    }
+    return result;
+}
+
+int main() {
+    int n;
+    cout << "Enter a number: ";
+    cin >> n;
+    cout << "Fibonacci(" << n << ") = " << fibonacci_iterative(n) << endl;
+    return 0;
+}
